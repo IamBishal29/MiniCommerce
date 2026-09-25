83230b66d1c1edeac30d9b8bcf57e706676baadf
SyedAhasanHabibBJIT <ahasan.habib@bjitgroup.com>
2026-09-25 15:54:37 +0600

    Initial project setup

---
+++ src/Services/ProductService/ProductService.Domain/Entities/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string? Description { get; private set; }
+
+    public decimal Price { get; private set; }
+
+    public int StockQuantity { get; private set; }
+
+    public DateTime CreatedAt { get; private set; }
+
+    public DateTime? UpdatedAt { get; private set; }
+
+    private Product()
+    {
+    }
+
+    public Product(
+        string name,
+        string? description,
+        decimal price,
+        int stockQuantity)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+        Price = price;
+        StockQuantity = stockQuantity;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
